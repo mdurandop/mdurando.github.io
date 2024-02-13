d2ac768df17c7198b25141670fb86ed0f77af3bd
mdurandop <mdurandop@gmail.com>
2024-02-12 21:34:12 -0500

    Galería terminada

--- script.js
+++ script.js
@@ -27,17 +27,109 @@ $( document ).ready(function() {
 
 function toggleImg1() {
     let cover = document.querySelector('.img1');
-    let signo = document.querySelector('.signo');
+    let signo = document.querySelector('.signo1');
     
     if (cover.style.backgroundImage === 'none') {
         cover.style.backgroundColor = '$accent';
     } else {
         cover.style.backgroundImage = 'url(app/img/galery/louis.png)';
-        cover.style.backgroundColor = '$accent';
+        cover.style.backgroundColor = 'transparent';
         signo.style.display = 'none';
     }
 }
 
-document.querySelector('.img').addEventListener('click', toggleImg1);
+document.querySelector('.img2').addEventListener('click', toggleImg2);
+
+function toggleImg2() {
+    let cover = document.querySelector('.img2');
+    let signo = document.querySelector('.signo2');
+    
+    if (cover.style.backgroundImage === 'none') {
+        cover.style.backgroundColor = '$accent';
+    } else {
+        cover.style.backgroundImage = 'url(app/img/galery/coca-cola.png)';
+        cover.style.backgroundColor = 'transparent';
+        signo.style.display = 'none';
+    }
+}
+
+document.querySelector('.img2').addEventListener('click', toggleImg2);
+
+function toggleImg3() {
+    let cover = document.querySelector('.img3');
+    let signo = document.querySelector('.signo3');
+    
+    if (cover.style.backgroundImage === 'none') {
+        cover.style.backgroundColor = '$accent';
+    } else {
+        cover.style.backgroundImage = 'url(app/img/galery/girasoles.png)';
+        cover.style.backgroundColor = 'transparent';
+        signo.style.display = 'none';
+    }
+}
+
+document.querySelector('.img3').addEventListener('click', toggleImg3);
+
+function toggleImg4() {
+    let cover = document.querySelector('.img4');
+    let signo = document.querySelector('.signo4');
+    
+    if (cover.style.backgroundImage === 'none') {
+        cover.style.backgroundColor = '$accent';
+    } else {
+        cover.style.backgroundImage = 'url(app/img/galery/1D.png)';
+        cover.style.backgroundColor = 'transparent';
+        signo.style.display = 'none';
+    }
+}
+
+document.querySelector('.img4').addEventListener('click', toggleImg4);
+
+function toggleImg5() {
+    let cover = document.querySelector('.img5');
+    let signo = document.querySelector('.signo5');
+    
+    if (cover.style.backgroundImage === 'none') {
+        cover.style.backgroundColor = '$accent';
+    } else {
+        cover.style.backgroundImage = 'url(app/img/galery/minion.png)';
+        cover.style.backgroundColor = 'transparent';
+        signo.style.display = 'none';
+    }
+}
+
+document.querySelector('.img5').addEventListener('click', toggleImg5);
+
+function toggleImg6() {
+    let cover = document.querySelector('.img6');
+    let signo = document.querySelector('.signo6');
+    
+    if (cover.style.backgroundImage === 'none') {
+        cover.style.backgroundColor = '$accent';
+    } else {
+        cover.style.backgroundImage = 'url(app/img/galery/iker.png)';
+        cover.style.backgroundColor = 'transparent';
+        signo.style.display = 'none';
+    }
+}
+
+document.querySelector('.img6').addEventListener('click', toggleImg6);
+
+function toggleImg7() {
+    let cover = document.querySelector('.img7');
+    let signo = document.querySelector('.signo7');
+    
+    if (cover.style.backgroundImage === 'none') {
+        cover.style.backgroundColor = '$accent';
+    } else {
+        cover.style.backgroundImage = 'url(app/img/galery/taylor.png)';
+        cover.style.backgroundColor = 'transparent';
+        signo.style.display = 'none';
+    }
+}
+
+document.querySelector('.img7').addEventListener('click', toggleImg7);
+
+
 
 
